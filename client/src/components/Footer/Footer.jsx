6bb19a0364b7914ea8d086c8be716a48ad806c1e
MehaletKB <mkesatebirhan@gmail.com>
2021-10-07 21:04:11 -0500

    :tada: header and footer files

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+
+const Footer = () => {
+  const location = useLocation();
+  const history = useHistory();
+  return (
+    <footer className="w-100 mt-auto bg-secondary p-4">
+      <div className="container text-center mb-5">
+        {location.pathname !== '/' && (
+          <button
+            className="btn btn-dark mb-3"
+            onClick={() => history.goBack()}
+          >
+            &larr; Go Back
+          </button>
+        )}
+        <h4>
+          Made by Dan, Ashley, Gabe & Mehalet
+        </h4>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
