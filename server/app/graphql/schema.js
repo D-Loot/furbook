6a0df10c37e82d5baef77046e95a6590119d8395
Dan Looten <danwlooten@gmail.com>
2021-10-07 19:44:34 -0500

    Add initial Schema code

--- server/app/graphql/schema.js
+++ server/app/graphql/schema.js
@@ -1,10 +1,62 @@
 import { gql } from "apollo-server-express";
 
 export default gql`
-  # TODO: 🙄
-  type Query {
-    helloWorld: String
+  type Post {
+    _id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+    comments: [Comment]
+    commentCount: Int!
+    goodBoys: [GoodBoy]
+    goodBoyCount: Int!
+    catKnips: [CatKnip]
+    catKnipCount: Int!
   }
-
-  # TODO: Add mutation(s) that return(s) a JWT.
+  type Comment {
+    commentId: ID!
+    createdAt: String!
+    username: String!
+    body: String!
+  }
+  type GoodBoy {
+    gbId: ID!
+    createdAt: String!
+    username: String!
+  }
+  type CatKnip {
+    ckId: ID!
+    createdAt: String!
+    username: String!
+  }
+  type User {
+    userId: ID!
+    email: String!
+    token: String!
+    username: String!
+    createdAt: String!
+  }
+  input RegisterInput {
+    username: String!
+    password: String!
+    confirmPassword: String!
+    email: String!
+  }
+  type Query {
+    getPosts: [Post]
+    getPost(postId: ID!): Post
+  }
+  type Mutation {
+    register(registerInput: RegisterInput): User!
+    login(username: String!, password: String!): User!
+    createPost(body: String!): Post!
+    deletePost(postId: ID!): String!
+    createComment(postId: String!, body: String!): Post!
+    deleteComment(postId: ID!, commentId: ID!): Post!
+    goodBoyPost(postId: ID!): Post!
+    likePost(postId: ID!): Post!
+  }
+  type Subscription {
+    newPost: Post!
+  }
 `;
