4a78e0d0d3202e8773b2e694cac3970023e90419
Dan Looten <danwlooten@gmail.com>
2021-10-10 20:22:49 -0500

    update index.js to include authentication

--- server/app/index.js
+++ server/app/index.js
@@ -7,33 +7,31 @@ const { authMiddleware } = require('./utils/auth');
 
 const db = require('./config/connection');
 
+const PORT = process.env.PORT || 3001;
 const app = express();
 
-const httpServer = http.createServer(app);
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-  // TODO: Add context using token service.
+  context: authMiddleware,
 });
 
-server
-  .start()
-  .then(() => {
-    server.applyMiddleware({ app });
+server.applyMiddleware({ app });
 
-    httpServer.listen({ port: 4000 }, () => {
-      console.info(
-        `🚀 Server ready at http://localhost:4000${server.graphqlPath}`
-      );
-    });
-  })
-  .catch((error) => {
-    console.error("Error starting server: ", error);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
-
-dbClient.connect();
-
-process.on("SIGINT", () => {
-  dbClient.close();
 });
