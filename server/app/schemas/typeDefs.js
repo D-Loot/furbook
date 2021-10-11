f28ee1362dfe309feaeb28848b5c76ab0b30ecf2
Dan Looten <danwlooten@gmail.com>
2021-10-10 20:47:43 -0500

    Add database layout in typeDefs, models, and auth files

--- server/app/schemas/typeDefs.js
+++ server/app/schemas/typeDefs.js
@@ -1,6 +1,34 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    posts: [Post]!
+  }
+
+  type Post {
+    _id: ID
+    postText: String
+    postAuthor: String
+    createdAt: String
+    comments: [Comment]!
+  }
+
+  type Comment {
+    _id: ID
+    commentText: String
+    commentAuthor: String
+    createdAt: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
 `;
 
 module.exports = typeDefs;
