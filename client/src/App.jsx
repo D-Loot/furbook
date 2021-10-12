29e9e2d00989da21ae573a6860088176de4ddbed
A Fish <ashley.paz.fish@gmail.com>
2021-10-11 21:00:28 -0500

    footer and css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,6 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "semantic-ui-css/semantic.min.css";
 import { Container } from "semantic-ui-react";
-// import "./App.css";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import { useHistory, useLocation } from 'react-router-dom';
+import React from "react";
+import { useHistory, useLocation } from "react-router-dom";
+import { Button } from "semantic-ui-react";
+import "./footer.css";
 
 const Footer = () => {
   const location = useLocation();
   const history = useHistory();
   return (
-    <footer className=" ">
-      <div className=" ">
-        {location.pathname !== '/' && (
-          <button
-            className=" "
+    <footer className="footer">
+      <div className="footerWrap">
+        {location.pathname !== "/" && (
+          <Button
+            className="ui button"
+            id="backBtn"
             onClick={() => history.goBack()}
           >
-            &larr; Go Back
-          </button>
+            <i className="fas fa-angle-left"></i> Go Back
+          </Button>
         )}
-        <h4>
-          Made by Dan, Ashley, Gabe & Mehalet
-        </h4>
+        <h4>Made by Dan, Ashley, Gabe & Mehalet</h4>
       </div>
     </footer>
   );
