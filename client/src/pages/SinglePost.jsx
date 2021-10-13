47c6d74fcb8b5cafdd7325ab76119fbe1c3dc2aa
A Fish <ashley.paz.fish@gmail.com>
2021-10-13 16:10:50 -0500

    single post page

--- client/src/pages/SinglePost.jsx
+++ client/src/pages/SinglePost.jsx
@@ -1,8 +1,55 @@
-import Component from "components/Component";
+import { useQuery } from "@apollo/client";
 import React from "react";
+// Import the `useParams()` hook
+import { useParams } from "react-router-dom";
+import CommentForm from "../components/CommentForm";
+import CommentList from "../components/CommentList";
+import { QUERY_SINGLE_POST } from "../utils/queries";
 
-function SinglePost() {
-  return <Component />;
-}
+const SinglePost = () => {
+  // Use `useParams()` to retrieve value of the route parameter `:profileId`
+  const { postId } = useParams();
+
+  const { loading, data } = useQuery(QUERY_SINGLE_POST, {
+    // pass URL parameter
+    variables: { postId },
+  });
+
+  const post = data?.post || {};
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div className="my-3">
+      <h3 className="card-header bg-dark text-light p-2 m-0">
+        {post.thoughtAuthor} <br />
+        <span style={{ fontSize: "1rem" }}>
+          had this thought on {post.createdAt}
+        </span>
+      </h3>
+      <div className="bg-light py-4">
+        <blockquote
+          className="p-4"
+          style={{
+            fontSize: "1.5rem",
+            fontStyle: "italic",
+            border: "2px dotted #1a1a1a",
+            lineHeight: "1.5",
+          }}
+        >
+          {post.postText}
+        </blockquote>
+      </div>
+
+      <div className="my-5">
+        <CommentList comments={post.comments} />
+      </div>
+      <div className="m-3 p-4" style={{ border: "1px dotted #1a1a1a" }}>
+        <CommentForm thoughtId={post._id} />
+      </div>
+    </div>
+  );
+};
 
 export default SinglePost;
