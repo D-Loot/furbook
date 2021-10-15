c2fe5396f47bfba052f111109ddc2ac674c60b2a
A Fish <ashley.paz.fish@gmail.com>
2021-10-14 21:40:52 -0500

    local host uri

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,7 +19,7 @@ import SinglePost from "./pages/SinglePost";
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
-  uri: "/graphql",
+  uri: "http://localhost:3001/graphql",
 });
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
